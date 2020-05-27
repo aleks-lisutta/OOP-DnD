22cb43a3ec1d28d765dd57ceb97a24a58859dcb0
yakov elmaleh <yakovelm@post.bgu.ac.il>
2020-05-27 18:58:30 +0300

    asd

---
+++ Documents/סמסטר ב/תכנות מונחה עצמים/DND/OOP-DnD-master/src/Entity/Player/Player.java
@@ -0,0 +1,7 @@
+package Entity.Player;
+
+import Entity.Heroic;
+import Entity.Tile.Unit;
+
+public abstract class Player extends Unit implements Heroic {
+}
---
+++ Documents/סמסטר ב/תכנות מונחה עצמים/DND/src/DnD.java
@@ -0,0 +1,5 @@
+public class DnD {
+    public static void main(String[] args){
+
+    }
+}
