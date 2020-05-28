196464143895611c735355b6e295a5f1fb8ec621
aleks <aleks.lisutta@gmail.com>
2020-05-28 14:47:36 +0300

    added service and controller dependancies

--- src/GameControl/Controller.java
+++ src/GameControl/Controller.java
@@ -1,4 +1,28 @@
 package GameControl;
 
+import Entity.Player.Player;
+import jdk.nashorn.internal.ir.IdentNode;
+
 public class Controller {
+    public IdentNode player;
+
+    public void loadBoards() {
+    }
+
+    public void setPlayer(Player player) {
+    }
+
+    public boolean finish() {
+        return false;
+    }
+
+    public String display() {
+        return "this should be the board and everything else";
+    }
+
+    public void action(String actionChar) {
+    }
+
+    public void enemyTurn() {
+    }
 }
